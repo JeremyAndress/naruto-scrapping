cf4d8a9c9f7b4b248f71123d247cc75723442e80
JeremySilvaSilva <jeremysilvasilva@gmail.com>
2020-07-30 10:40:09 -0400

    STYLE: remove commentary:construction:

--- src/justus.ts
+++ src/justus.ts
@@ -1,19 +1,8 @@
 
 export async function get_all_justus(page:any){
-    // const data = await page.evaluate(() => { 
-    //     const category = document.querySelector("#mw-content-text > div.category-page__members");
-    //     if (category){
-    //         // return category.textContent
-    //         const jutsus = Array.from(category.querySelectorAll('ul.category-page__members-for-char'));
-    //         return jutsus
-    //     }else{
-    //         return {}
-    //     }
-    // });
     const data = await page.evaluate(() => { 
         const all_link = document.querySelectorAll('a.category-page__member-link')
         const all_link_l = Array.from(all_link)
-        //return all_link_l.map(link => link.textContent);
         return all_link_l.map(link => {
             return {
                 href:link.getAttribute('href'),
